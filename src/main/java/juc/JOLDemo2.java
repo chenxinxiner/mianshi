d18ab274f809d8c3a53effb2b252c3d603d65dd0
chenxinxiner <1074537862@qq.com>
2021-07-06 16:40:18 +0800

    feat: 锁本质查看

---
+++ src/main/java/juc/JOLDemo2.java
@@ -0,0 +1,20 @@
+package juc;
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.concurrent.TimeUnit;
+
+public class JOLDemo2 {
+    private static Object  o;
+    public static void main(String[] args) {
+        o = new Object();
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized (o){
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
+    }
+}
