c4a849fcea3c319eaffa39ac8a4f2e0c6370a54e
chenxinxiner <1074537862@qq.com>
2021-07-12 22:56:29 +0800

    feat:LockSupport

---
+++ src/main/java/juc/locksupporttest/LockSupportTest.java
@@ -0,0 +1,34 @@
+package juc.locksupporttest;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * LockSupport学习
+ *
+ * @author ：chenxin
+ * @date ：Created in 2021/7/12 21:54
+ */
+public class LockSupportTest {
+
+    public static void main(String[] args) {
+        Thread a = new Thread(() ->{
+            System.out.println(Thread.currentThread().getName()+"线程等待");
+            LockSupport.park();
+            System.out.println(Thread.currentThread().getName()+"线程被唤醒");
+
+        },"a");
+        Thread b = new Thread(() ->{
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LockSupport.unpark(a);
+            System.out.println(Thread.currentThread().getName()+"线程Unpark");
+        },"b");
+
+        a.start();
+        b.start();
+    }
+}
