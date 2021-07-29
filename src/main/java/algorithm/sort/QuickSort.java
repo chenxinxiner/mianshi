6bb4a3d09bfa613a882ba34a9c2dc6783253e015
chenxinxiner <1074537862@qq.com>
2021-07-29 23:09:24 +0800

    feat:快速排序完善

--- src/main/java/algorithm/sort/QuickSort.java
+++ src/main/java/algorithm/sort/QuickSort.java
@@ -1,5 +1,7 @@
 package algorithm.sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
     public static <T extends Comparable<T>> void sort(T[] array) {
@@ -7,14 +9,47 @@ public class QuickSort {
     }
 
     private static <T extends Comparable<T>> void sort(T[] array,int left,int right) {
-
+        if (left >= right) return;
+        int l = partition(array, left, right);
+        sort(array,left,l);
+        sort(array,l+1,right);
     }
 
-    private static <T extends Comparable<T>> void partition(T[] array, int left,int right) {
-        int mid = (right+left) >>> 1;
+    private static <T extends Comparable<T>> int partition(T[] array, int left,int right) {
+        int i = left+1;
+        int j = right;
+        T t = array[left];
+        while (true) {
+            while (t.compareTo(array[i]) > 0) {
+                i++;
+                if (i == right) {
+                    break;
+                }
+            }
+            while (t.compareTo(array[j]) < 0) {
+                j--;
+                if (j == left) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            swap(array,i,j);
+        }
+        swap(array,left,j);
+        return j;
+    }
 
+    public static <T extends Comparable<T>> void swap(T[] array, int index1, int index2) {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 
     public static void main(String[] args) {
+        Integer[] l = new Integer[]{10,26554,33,45,13,65,87,234,64,6577,123,765};
+        sort(l);
+        System.out.println(Arrays.toString(l));
     }
 }
